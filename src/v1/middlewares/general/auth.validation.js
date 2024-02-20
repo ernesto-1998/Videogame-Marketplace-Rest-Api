a0fb976e19491c1e49127ab52548f8b35af9ddec
ernesto-1998 <ernestoalonso1998@gmail.com>
2024-02-20 16:10:05 -0600

    Updating auth.validation to correct http codes responses

--- src/v1/middlewares/general/auth.validation.js
+++ src/v1/middlewares/general/auth.validation.js
@@ -1,9 +1,11 @@
+import STATUS from "../../enums/status-keys.js"
+
 export const isUserActive = (req, res, next) => {
     if (req.session.user) {
         next()
     } else {
-        res.status(400).json({
-            status: 'fail',
+        res.status(401).json({
+            status: STATUS.ERROR,
             message: 'You need to login in order to complete this action...',
         })
     }
@@ -14,7 +16,7 @@ export const isUserInactive = (req, res, next) => {
         next()
     } else {
         res.status(400).json({
-            status: 'fail',
+            status: STATUS.ERROR,
             message:
                 'You have an active user, please logout in order to complete this action...',
         })
@@ -25,8 +27,8 @@ export const isUserAdmin = (req, res, next) => {
     if (req.session.user.roleId === 1) {
         next()
     } else {
-        res.status(401).json({
-            status: 'fail',
+        res.status(403).json({
+            status: STATUS.ERROR,
             message: 'This resource requires admin privileges',
         })
     }
